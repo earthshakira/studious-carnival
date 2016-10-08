de73af891d18aa79aadbe92a8214657236bd34d4
earthshakira <arawkar.shubham08@gmail.com>
2016-10-08 10:39:49 +0530

    12478 - Hardest Problem ...

---
+++ 12478 - Hardest Problem .../src/com/iridium/Main.java
@@ -0,0 +1,13 @@
+package com.iridium;
+
+/*
+    18140662 	12478 	Hardest Problem Ever (Easy) 	Accepted 	JAVA 	0.030 	2016-10-08 05:09:07
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("KABIR");
+    }
+}
